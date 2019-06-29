ebcda459bbd376c5b4080e32dcd8104206f36eef
sidiousvic <victor6rene@gmail.com>
2019-06-29 23:57:29 +0900

    Initial upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const pokedex = new Pokedex();
+const ui = new UI();
+
+// search input
+const searchPoke = document.getElementById("searchPoke");
+
+// search input event listener
+searchPoke.addEventListener("keyup", e => {
+  // document.addEventListener("DOMContentLoaded", e => {
+  // get input text
+  const pokeText = e.target.value;
+  // const pokeText = "gengar";
+  // if input is empty, proceed
+  if (pokeText !== "") {
+    // make http call
+    pokedex.getPoke(pokeText).then(data => {
+      // show profile
+      ui.renderPoke(data);
+    });
+  } else {
+    //clear profile
+    ui.clearEntry();
+  }
+});
