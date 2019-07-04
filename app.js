2f5dfcc963c61b88e5896522165f1824c3add6f5
sidiousvic <victor6rene@gmail.com>
2019-07-04 22:06:17 +0900

    Added prev/next functionality

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const ui = new UI();
 
 // search input
 const searchPoke = document.getElementById("searchPoke");
+const nextButton = document.querySelector(".next-button");
+const prevButton = document.querySelector(".prev-button");
 
 // search input event listener
-// searchPoke.addEventListener("keyup", e => {
-document.addEventListener("DOMContentLoaded", e => {
+searchPoke.addEventListener("keyup", e => {
+  // document.addEventListener("DOMContentLoaded", e => {
   // get input text
-  // const pokeText = e.target.value.toLowerCase();
-  const pokeText = "gengar";
+  const pokeText = e.target.value.toLowerCase();
+  // const pokeText = "gengar";
   // if input is empty, proceed
   if (pokeText !== "") {
     // make http call
@@ -21,4 +23,32 @@ document.addEventListener("DOMContentLoaded", e => {
     //clear profile
     ui.clearEntry();
   }
+});
+
+// next button event listener
+nextButton.addEventListener("click", () => {
+  // store current search text
+  const currentPokeText = searchPoke.value.toLowerCase();
+  // get current pokemon data
+  pokedex.getPoke(currentPokeText).then(data => {
+    pokedex.getPoke(data.id + 1).then(data => {
+      searchPoke.value = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+      // show profile
+      ui.renderPoke(data);
+    });
+  });
+});
+
+// next button event listener
+prevButton.addEventListener("click", () => {
+  // store current search text
+  const currentPokeText = searchPoke.value.toLowerCase();
+  // get current pokemon data
+  pokedex.getPoke(currentPokeText).then(data => {
+    pokedex.getPoke(data.id - 1).then(data => {
+      searchPoke.value = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+      // show profile
+      ui.renderPoke(data);
+    });
+  });
 });
